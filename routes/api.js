ad033120469fd004d44845c9dc1f0ed135ee706d
Abel Majadibodu <Abeljackson33@gmail.com>
2024-07-13 13:07:07 +0200

    update

--- routes/api.js
+++ routes/api.js
@@ -1,59 +1,68 @@
 "use strict";
 
+let books = [
+  { _id: 1, title: "Book 1", commentcount: 0, comments: [] },
+  { _id: 2, title: "Book 2", commentcount: 0, comments: [] },
+  { _id: 3, title: "Book 3", commentcount: 0, comments: [] },
+];
+
 module.exports = function (app) {
   app
     .route("/api/books")
     .get(function (req, res) {
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-      let books = [
-        { _id: 1, title: "Book 1", commentcount: 0 },
-        { _id: 2, title: "Book 2", commentcount: 0 },
-        { _id: 3, title: "Book 3", commentcount: 0 },
-      ];
       res.json(books);
     })
-
     .post(function (req, res) {
       let title = req.body.title;
-      //response will contain new book object including atleast _id and title
-      let book = { _id: Date.now(), title: title, commentcount: 0 };
-      res.json(book);
+      if (!title) {
+        return res
+          .status(400)
+          .json({ message: "missing required field title" });
+      }
+      let newBook = {
+        _id: Date.now(),
+        title: title,
+        commentcount: 0,
+        comments: [],
+      };
+      books.push(newBook);
+      res.json(newBook);
     })
-
     .delete(function (req, res) {
-      //if successful response will be 'complete delete successful'
+      books = [];
       res.json({ message: "complete delete successful" });
     });
 
   app
     .route("/api/books/:id")
     .get(function (req, res) {
-      let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
-      let book = {
-        _id: bookid,
-        title: "Book " + bookid,
-        comments: [],
-      };
+      let book = books.find((b) => b._id === parseInt(req.params.id));
+      if (!book) {
+        return res.status(404).json({ message: "no book exists" });
+      }
       res.json(book);
     })
-
     .post(function (req, res) {
-      let bookid = req.params.id;
+      let book = books.find((b) => b._id === parseInt(req.params.id));
+      if (!book) {
+        return res.status(404).json({ message: "no book exists" });
+      }
       let comment = req.body.comment;
-      //json res format same as .get
-      let book = {
-        _id: bookid,
-        title: "Book " + bookid,
-        comments: comment ? [comment] : [],
-      };
+      if (!comment) {
+        return res
+          .status(400)
+          .json({ message: "missing required field comment" });
+      }
+      book.comments.push(comment);
+      book.commentcount++;
       res.json(book);
     })
-
     .delete(function (req, res) {
-      let bookid = req.params.id;
-      //if successful response will be 'delete successful'
+      let index = books.findIndex((b) => b._id === parseInt(req.params.id));
+      if (index === -1) {
+        return res.status(404).json({ message: "no book exists" });
+      }
+      books.splice(index, 1);
       res.json({ message: "delete successful" });
     });
 };
